700a39a7b3cef662254d0d6ef90a52c93b7fae4d
Zowye <caiozowye@gmail.com>
2024-09-06 15:24:40 -0300

    Removed the logo

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,5 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
     Oie
   </q-page>
 </template>
@@ -15,4 +10,4 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'IndexPage'
 })
-</script>
+</script>   
